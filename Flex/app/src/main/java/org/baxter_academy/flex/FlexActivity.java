09f1d45b27549c893b529f6d257e1df46e0d95ff
wilstenholme <woraman.lo@gmail.com>
2016-01-10 15:39:07 -0500

    Add initial files for Flex

---
+++ Flex/app/src/main/java/org/baxter_academy/flex/FlexActivity.java
@@ -0,0 +1,32 @@
+package org.baxter_academy.flex;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FlexActivity extends AppCompatActivity {
+    TextView textView;
+
+    public void helloWorld(View view){
+        String button_text;
+        button_text = ((Button) view).getText().toString();
+        if(button_text.equals("Open Minor Activity")){
+            Intent intent = new Intent(this, MinorActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void sendMessage(){
+        //
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flex_layout);
+        textView = (TextView)findViewById(R.id.greetings_text_view);
+    }
+}
---
+++ Flex/app/src/main/java/org/baxter_academy/flex/MinorActivity.java
@@ -0,0 +1,15 @@
+package org.baxter_academy.flex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wil on 1/12/16.
+ */
+public class MinorActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.minor_layout);
+    }
+}
