9af7742a131b42349402b8bfb5ee478d60b3e674
wilstenholme <woraman.lo@gmail.com>
2016-01-17 18:38:08 -0500

    Force rename addtask

--- Flex/app/src/main/java/org/baxter_academy/flex/AddTaskActivity.java
+++ Flex/app/src/main/java/org/baxter_academy/flex/AddTaskActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
