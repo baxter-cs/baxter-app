0db166b9a13ad279721155f8e3930e705c2494a2
wilstenholme <woraman.lo@gmail.com>
2016-01-18 00:15:36 -0500

    Create blank fragments

---
+++ Flex/app/src/main/java/org/baxter_academy/flex/TodoFragment.java
@@ -0,0 +1,27 @@
+package org.baxter_academy.flex;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TodoFragment extends Fragment {
+
+    public TodoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo, container, false);
+    }
+
+}
