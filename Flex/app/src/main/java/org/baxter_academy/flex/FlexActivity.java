e300020e9d86088c837d32bcb4354108916a3c87
wilstenholme <woraman.lo@gmail.com>
2016-01-18 14:48:45 -0500

    Remove unused imports

--- Flex/app/src/main/java/org/baxter_academy/flex/FlexActivity.java
+++ Flex/app/src/main/java/org/baxter_academy/flex/FlexActivity.java
@@ -2,7 +2,6 @@ package org.baxter_academy.flex;
 
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,10 +10,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.widget.Button;
-
-import java.lang.reflect.Field;
 
 public class FlexActivity extends AppCompatActivity {
 
