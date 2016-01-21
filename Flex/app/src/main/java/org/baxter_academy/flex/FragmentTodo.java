14ded571e7e192b614c53f42c8205feae2ce7506
wilstenholme <woraman.lo@gmail.com>
2016-01-20 19:26:52 -0500

    Add linear layout programmatically on onCreateView

--- Flex/app/src/main/java/org/baxter_academy/flex/FragmentTodo.java
+++ Flex/app/src/main/java/org/baxter_academy/flex/FragmentTodo.java
@@ -5,11 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.Serializable;
 
 public class FragmentTodo extends Fragment {
+
+    LinearLayout linearLayout;
 
     public FragmentTodo() {
         // Required empty public constructor
@@ -23,18 +26,37 @@ public class FragmentTodo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_todo, container, false);
+
         FlexActivity flexActivity = (FlexActivity) getActivity();
         Serializable mTask = flexActivity.getTask();
         Task task = (Task) mTask;
-
         if(task != null){
-            TextView tv = (TextView) view.findViewById(R.id.testing);
-            tv.setText(task.toString());
-        }
 
+            TextView tv = (TextView) view.findViewById(R.id.title_todo);
+            tv.setText(task.toString());
+
+            //TODO: Begins
+            LinearLayout layout = (LinearLayout) view.findViewById(R.id.todo_layout);
+            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            linearLayout = new LinearLayout(getActivity());
+            linearLayout.setOrientation(LinearLayout.VERTICAL);
+            linearLayout.setLayoutParams(layoutParams);
+
+            TextView taskTitle = new TextView(getActivity());
+            taskTitle.setLayoutParams(layoutParams);
+            taskTitle.setText(task.getTaskTitle());
+            linearLayout.addView(taskTitle);
+            layout.addView(linearLayout);
+
+            //TODO: Ends
+
+        }
         return view;
+    }
+
+    public void createTask(){
+
     }
 
 }
