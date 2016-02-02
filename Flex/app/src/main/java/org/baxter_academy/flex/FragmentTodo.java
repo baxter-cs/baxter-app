7e9d4f2541e0894aa1d3d6e4de2227600884323e
JCharante <ralphcharante@gmail.com>
2016-02-02 13:08:23 -0500

    Proof of Concept Delete Button

--- Flex/app/src/main/java/org/baxter_academy/flex/FragmentTodo.java
+++ Flex/app/src/main/java/org/baxter_academy/flex/FragmentTodo.java
@@ -2,19 +2,24 @@ package org.baxter_academy.flex;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class FragmentTodo extends Fragment {
 
@@ -70,7 +75,39 @@ public class FragmentTodo extends Fragment {
                 textView.setBackgroundColor(Color.parseColor("#F8BBD0"));
                 textView.setPadding(12, 12, 12, 12);
                 textView.setTextColor(Color.parseColor("#515151"));
+                textView.setMovementMethod(new ScrollingMovementMethod());
                 linearLayout.addView(textView);
+
+                Button deleteButton = new Button(getActivity());
+                deleteButton.setText("Delete");
+                deleteButton.setTag(task.getTaskTitle());
+                deleteButton.setOnClickListener(new View.OnClickListener() {
+                    // This is run when the Button is pressed
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences prefs = getActivity().getSharedPreferences("meta", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        String json = prefs.getString("tasks", "error");
+                        Gson gson = new Gson();
+                        TaskStorage task_storage = gson.fromJson(json, TaskStorage.class);
+                        List<Task> filteredTasks = new ArrayList<Task>();
+                        for(Iterator<Task> i = task_storage.tasks.iterator(); i.hasNext();) {
+                            Task filteredTask = i.next();
+                            // TODO: Have IDs for each task because this will get rid of everything w/ the same title
+                            if (!filteredTask.getTaskTitle().equals(v.getTag())) {
+                                filteredTasks.add(filteredTask);
+                            }
+                        }
+                        task_storage.tasks = filteredTasks;
+                        // Saves the updated Task Storage
+                        editor.putString("tasks", gson.toJson(task_storage));
+                        editor.commit();
+                        Intent intent = new Intent(getContext(), FlexActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                linearLayout.addView(deleteButton);
+
                 layout.addView(linearLayout);
             }
             tv.setText(foobar);
