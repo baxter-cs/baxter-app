34f194e644faba98e908578d8f57047a3dedd6c8
JCharante <ralphcharante@gmail.com>
2016-02-04 22:34:20 -0500

    Finished tutorial three

--- Flex/app/src/main/java/org/baxter_academy/flex/TutorialThree.java
+++ Flex/app/src/main/java/org/baxter_academy/flex/TutorialThree.java
@@ -17,33 +17,33 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class TutorialTwo extends AppCompatActivity {
+public class TutorialThree extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tutorial_two_layout);
-        TextView textViewInfo = (TextView) findViewById(R.id.todoInfoTaskExample);
+        setContentView(R.layout.tutorial_three_layout);
+        TextView textViewInfo = (TextView) findViewById(R.id.doingInfoTaskExample);
         textViewInfo.setClickable(true);
         textViewInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
                 // Inflate the popup menu with xml file
-                popupMenu.getMenuInflater().inflate(R.menu.menu_todo, popupMenu.getMenu());
+                popupMenu.getMenuInflater().inflate(R.menu.menu_doing, popupMenu.getMenu());
 
                 // Add popupMenu with onMenuItemClickListener
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
                         switch (item.getItemId()) {
-                            case R.id.move_to_doing:
-                                Toast.makeText(v.getContext(), "Task Moved to In Process", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(v.getContext(), TutorialThree.class);
+                            case R.id.move_to_done:
+                                Toast.makeText(v.getContext(), "Task Moved to Done", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(v.getContext(), TutorialFour.class);
                                 startActivity(intent);
                                 break;
                             case R.id.delete:
-                                Toast.makeText(v.getContext(), "Sorry, you can't delete this Task right now", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(v.getContext(), "You cannot delete this task yet", Toast.LENGTH_SHORT).show();
                                 break;
                             default:
                                 Toast.makeText(v.getContext(), item.getTitle() + " Clicked", Toast.LENGTH_SHORT).show();
