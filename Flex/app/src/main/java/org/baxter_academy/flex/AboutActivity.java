f941786b83131db9139af089e982a8e672eaadc2
wilstenholme <woraman.lo@gmail.com>
2016-02-04 23:14:47 -0500

    Hyperlink to GitHub repository

--- Flex/app/src/main/java/org/baxter_academy/flex/AboutActivity.java
+++ Flex/app/src/main/java/org/baxter_academy/flex/AboutActivity.java
@@ -2,6 +2,8 @@ package org.baxter_academy.flex;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity{
 
@@ -9,5 +11,8 @@ public class AboutActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_layout);
+
+        TextView src = (TextView) findViewById(R.id.text_version);
+        src.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
